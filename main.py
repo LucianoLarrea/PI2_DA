bb4b98c10357ac2eb3af004134d1fc032cd5b82b
Luciano Larrea <takticflow@gmail.com>
2023-03-23 13:12:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+# Crear un título para la aplicación
+st.set_page_config(page_title='SP500') # Nombre para configurar la pagina web
+st.header('Data Analysis') # Titulo de la pagina
